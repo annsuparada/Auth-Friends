34670049e4b2a3680b62288a8e8ae54c1160f4a7
Suparada Saitalae <ann.suparada@gmail.com>
2019-08-20 15:24:35 -0500

    first commit

---
+++ friends/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Login from './components/Login'
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Link to="/login" >Login</Link>
+      <Route path="/login" component={Login} />
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ friends/src/components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+class Login extends React.Component {
+    render() {
+        return(
+            <div>
+                <form>
+                    <input />
+                    <input />
+                    <button>Log in</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login;
